c87c46048c0be78136e7a05c79fa10ac12cff64f
majidalykhan <majidalykhan@gmail.com>
2021-06-03 19:07:19 +0500

    default-props

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,17 @@
-const Header = () => {
+const Header = ({title,paragraph}) => {
     return (
         <div>
             <header>
-                <h1>Task Tracker</h1>
+                <h1>{title}</h1>
+                <p>{paragraph}</p>
             </header>
         </div>
     )
+}
+
+Header.defaultProps = {
+    title: 'Task Tracker',
+    paragraph: "Hello this is task tracker react app.",
 }
 
 export default Header
