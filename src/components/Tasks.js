6596e949f65e22731feb704e32058b661ed233b7
majidalykhan <majidalykhan@gmail.com>
2021-06-05 11:28:30 +0500

    Tasks component

---
+++ src/components/Tasks.js
@@ -0,0 +1,35 @@
+const tasks = [
+    {
+        id: 1,
+        text: 'Eat',
+        day: '6th June at 8 PM',
+        reminder: true,
+    },
+
+    {
+        id: 2,
+        text: 'Study',
+        day: '6th June at 10 PM',
+        reminder: true,
+    },
+
+    {
+        id: 3,
+        text: 'Sleep',
+        day: '6th June at 12 AM',
+        reminder: false,
+    },
+
+
+
+]
+
+const Tasks = () => {
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Tasks
